323ee789fe2a756cde3812438ac1a29a8d013075
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2023-03-06 10:18:22 -0300

    Resource entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,5 +15,9 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://live.staticflickr.com/65535/51922705627_5f861f6b15_b.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
 
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, couse_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, couse_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1)
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('HMTL trail', 'Main trail of the course', 1, 'https://live.staticflickr.com/65535/51922705627_5f861f6b15_b.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Clear your doubt', 2, 'https://live.staticflickr.com/65535/51922705627_5f861f6b15_b.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Exclusive lives to the class', 3, 'https://live.staticflickr.com/65535/51922705627_5f861f6b15_b.jpg', 0, 1);
