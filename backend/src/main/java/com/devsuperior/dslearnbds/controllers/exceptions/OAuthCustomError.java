df1ed03cd928e9ea8814849dd5f726b90c061ce8
Cairo Augusto de Andrade <cairo.andrade777@gmail.com>
2023-03-10 11:38:13 -0300

    Custom server level authorization

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/controllers/exceptions/OAuthCustomError.java
@@ -0,0 +1,40 @@
+package com.devsuperior.dslearnbds.controllers.exceptions;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class OAuthCustomError implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String error;
+	
+	@JsonProperty("error_descrption")
+	private String errorDescrption;
+	
+	public OAuthCustomError() {
+	}
+
+	public OAuthCustomError(String error, String errorDescrption) {
+		super();
+		this.error = error;
+		this.errorDescrption = errorDescrption;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public String getErrorDescrption() {
+		return errorDescrption;
+	}
+
+	public void setErrorDescrption(String errorDescrption) {
+		this.errorDescrption = errorDescrption;
+	}
+}
